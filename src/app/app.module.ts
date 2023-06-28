81f5998064b4d0597bdb5b028724141331d21431
luan_sevla <luanalvess01@gmail.com>
2023-06-28 17:33:18 -0300

    angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './shared/services/material.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
